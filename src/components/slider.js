a02b4d3a1459f4f67492989c82c90a1b4f13e648
samsvk <samsvk00@gmail.com>
2022-08-20 16:33:46 +0100

    updated display

--- src/components/slider.js
+++ src/components/slider.js
@@ -6,15 +6,28 @@ import "swiper/css/navigation";
 import "swiper/css/bundle";
 import Img1 from "../assets/1.jpg";
 import Img2 from "../assets/2.png";
+import Img3 from "../assets/3.jpg";
+import Img4 from "../assets/4.png";
+import { AiOutlineArrowRight } from "react-icons/ai";
 
 export default function Slider() {
   const views = [
     { url: Img2, name: "Gloves", link: "" },
+    { url: Img4, name: "Guess", link: "" },
+    { url: Img3, name: "Carrot", link: "" },
     { url: Img1, name: "Dreams", link: "" },
   ];
   return (
     <div className="slider">
-      <div className="next">1</div>
+      <div className="prev">
+        <AiOutlineArrowRight
+          style={{
+            verticalAlign: "middle",
+            transform: "rotate(180deg)",
+          }}
+        />
+      </div>
+
       <Swiper
         slidesPerView={1}
         loop={true}
@@ -42,7 +55,14 @@ export default function Slider() {
           </SwiperSlide>
         ))}
       </Swiper>
-      <div className="prev">2</div>
+      <div className="next">
+        {" "}
+        <AiOutlineArrowRight
+          style={{
+            verticalAlign: "middle",
+          }}
+        />
+      </div>
     </div>
   );
 }
