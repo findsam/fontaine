00f07c4c2c1dfa7ef1ca39d95fd0da02c55cc438
samsvk <samsvk00@gmail.com>
2022-08-21 09:48:31 +0100

    whitespace update

--- src/components/slider.js
+++ src/components/slider.js
@@ -80,7 +80,6 @@ export default function Slider() {
         ))}
       </Swiper>
       <div className="next">
-        {" "}
         <AiOutlineArrowRight
           style={{
             verticalAlign: "middle",
